a67a82655f0146de6bd46adb87d3dcf655e24cb8
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-07-10 00:21:15 +0300

    scaffold auth management pages

---
+++ Data/Migrations/20230702160508_AddImageTypeToProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace allinibp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageTypeToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageType",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageType",
+                table: "Products");
+        }
+    }
+}
